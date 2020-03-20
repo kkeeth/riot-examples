516af374f6715189fb6e83ddfc8587bfa9d9e669
kkeeth <zensin0082@gmail.com>
2020-03-21 00:05:43 +0900

    feat: error handling about giphy app

--- giphy-app/utils/api.js
+++ giphy-app/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import dotenv from 'dotenv'
 dotenv.config()
+
 const searchGifs = (query) => {
   // API key
   const key = process.env.API_KEY
@@ -10,15 +11,13 @@ const searchGifs = (query) => {
   const limit = 30
   // API request URL
   const reqUrl = `${baseUrl}?api_key=${key}&q=${query}&limit=${limit}rating=G`
+  // for debugging or Network error occurred
   // const jsonUrl = "http://localhost:1234/assets/response.json"
 
   return axios
     .get(reqUrl)
     .then(({ data: res }) => {
       return res.data
-    })
-    .catch(e => {
-      return e
     })
 }
 
