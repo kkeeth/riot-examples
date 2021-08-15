b08ed2dcb3629e9bede89eeee53b719971d84534
kkeeth <zensin0082@gmail.com>
2021-08-15 16:52:04 +0900

    fix: removed v6 demo because official teams get the demo of v6 with typescript ready

--- v6/src/components/global/sidebar/types.ts
+++
@@ -1,6 +0,0 @@
-export interface MyComponentState {
-  name: string;
-  showUser: boolean;
-}
-
-export interface MyComponentProps {}
--- v6/src/pages.js
+++
@@ -1,9 +0,0 @@
-export default [{
-  path: '/',
-  label: 'Home',
-  componentName: 'home'
-}, {
-  path: '/about',
-  label: 'About',
-  componentName: 'about'
-}]
