400d7cd042ce3580da660d92a0636c9acb2e0e7b
kkeeth <zensin0082@gmail.com>
2021-08-15 23:08:26 +0900

    chore: upgraded node modules

--- animation-demo/src/main.js
+++ animation-demo/src/main.js
@@ -1,8 +1,8 @@
 import '@riotjs/hot-reload'
 import {component} from 'riot'
 import App from './app.riot'
-// import App from './list.riot'
-// import App from './list-sample.riot'
+//import App from './list.riot'
+//import App from './list-sample.riot'
 
 component(App)(document.getElementById('app'), {
   title: 'Riot.js with Anime.js!'
